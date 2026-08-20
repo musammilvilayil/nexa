0a049baa7df6324ed9e99d168c223d7ea2020cb1
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-08-20 11:38:31 +0530

    export autonomous learning components

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,5 +1,14 @@
 """Bounded teacher-student training and skill-forge components."""
 
+from .autonomy import (
+    AutonomousLearningSummary,
+    AutonomousLearningSupervisor,
+    CapabilityGap,
+    DeficiencyDetector,
+    ImprovementBacklog,
+    LearningWeakness,
+)
+from .candidate_store import CandidateStore
 from .curriculum import CurriculumModule, TradingCurriculum
 from .engine import AutonomousTrainingEngine, TrainingRunSummary
 from .skill_forge import ForgeResult, SkillCandidate, SkillForge
@@ -7,10 +16,17 @@ from .store import TrainingStore
 from .trading_teacher import GeminiTradingTeacher, TradingLesson
 
 __all__ = [
+    "AutonomousLearningSummary",
+    "AutonomousLearningSupervisor",
     "AutonomousTrainingEngine",
+    "CandidateStore",
+    "CapabilityGap",
     "CurriculumModule",
+    "DeficiencyDetector",
     "ForgeResult",
     "GeminiTradingTeacher",
+    "ImprovementBacklog",
+    "LearningWeakness",
     "SkillCandidate",
     "SkillForge",
     "TradingCurriculum",
